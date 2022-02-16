481cd8d51473d3ab1158cd71fca2149cf67afae8
Matheus Vaz <matheusvaz@matheuss-air.lan>
2022-02-16 12:18:30 -0800

    requirement 3

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Header extends Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <div>
+        <h3>Trybe Wallet</h3>
+        <section>
+          <p data-testid="email-field">{ email }</p>
+          <p data-testid="total-field">0</p>
+          <p data-testid="header-currency-field">BRL</p>
+        </section>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Header from '../components/Header';
 
-class Wallet extends React.Component {
+class Wallet extends Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div>
+        <Header />
+      </div>
+    );
   }
 }
 
